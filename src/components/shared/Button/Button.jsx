f6e625e4cd76ab184225de0dbb2dc5806be10bf7
Luisil41 <luisisarrial@gmail.com>
2021-09-18 21:01:46 +0200

    FEAT: new files added

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -5,7 +5,6 @@ export const Button = ({type, className, children}) => {
     return (
         <>
             <button type={type} className={className}>{children}</button>
-            
         </>
     )
 }
