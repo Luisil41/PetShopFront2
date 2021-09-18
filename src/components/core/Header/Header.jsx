31c4a68095ca394940a4b2f479abf989401dd075
Guille <dende8bruselas@gmail.com>
2021-09-18 16:14:54 +0200

    Header and Menu styles

--- src/components/core/Header/Header.jsx
+++ src/components/core/Header/Header.jsx
@@ -11,9 +11,6 @@ export const Header = (props) => {
                 <span className="logo"><FontAwesomeIcon icon={faPaw} /></span>
                 <h1 className="title">{props.title}</h1>
             </div>
-            <div className="image__container">
-                <img className="image" src="https://cdn.cienradios.com/wp-content/uploads/sites/2/2020/07/D%C3%ADa-mundial-del-perro-c%C3%B3mo-ser-el-mejor-due%C3%B1o-de-una-mascota-kk.jpg" alt="mascota y dueño" />
-            </div>
             <div className="text__container">
                 <p className="text">¡Únete a nuestra comunidad y ayuda a las mascotas más necesitadas!</p>
             </div>
